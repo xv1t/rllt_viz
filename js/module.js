403ac2c6c94c045377abc70339ed30b5f106aa1b
xv1t <xv1t@yandex.ru>
2015-09-03 23:18:07 +0400

    Autocommit from rec at Чт. сент.  3 23:18:07 SAMT 2015

--- js/module.js
+++ js/module.js
@@ -1,10 +1,6 @@
 
-
-function maximizeme(){ window.moveTo(0, 0); window.resizeTo(screen.width, screen.height) }
-
 //document.write(new Date())
 console.log(new Date())
 setTimeout(function(){
     location.reload(false)
 }, 60000)
-maximizeme()
