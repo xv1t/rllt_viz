54be1ad5b1433ff064dc31b543bf18521a87ae81
xv1t <xv1t@yandex.ru>
2015-09-03 23:11:33 +0400

    Autocommit from rec at Чт. сент.  3 23:11:33 SAMT 2015

--- js/module.js
+++ js/module.js
@@ -1,12 +1,10 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+
+function maximizeme(){ window.moveTo(0, 0); window.resizeTo(screen.width, screen.height) }
 
 //document.write(new Date())
 console.log(new Date())
 setTimeout(function(){
     location.reload(false)
-}, 5000)
+}, 60000)
+maximizeme()
