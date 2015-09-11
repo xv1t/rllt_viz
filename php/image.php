906c13eb50c31b7ab1ea981bca9fe8a9b65d62cd
xv1t <you@example.com>
2015-09-11 12:04:19 +0400

    Autocommit from ltex01 at Пт. сент. 11 12:04:19 SAMT 2015

---
+++ php/image.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+
+require 'RLLT2300Parser.php';
+require 'RLLTApplication.php';
+require 'RLLTImage.php';
+
+
+$rllt_image = new RLLTImage();
+
+$rllt_image->testdraw("/home/xvit/tmp/test.jpg");
+//color(1, 2);
+
